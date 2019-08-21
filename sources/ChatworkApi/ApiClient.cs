c23a5e51919353d6d3249257cccb9c49bd4fbf11
iyemon018@gmail.com <iyemon018>
2019-08-21 23:02:42 +0900

    ソリューションの配置されていたフォルダを１階層上げた。

---
+++ sources/ChatworkApi/ApiClient.cs
@@ -0,0 +1,49 @@
+﻿namespace ChatworkApi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    internal sealed class ApiClient
+    {
+        private static readonly string BaseUri = "https://api.chatwork.com/v2";
+
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(string apiToken)
+        {
+            _httpClient = new HttpClient(new AuthenticationHandler(apiToken));
+        }
+
+        public async Task<string> GetAsync(string path, params (string key, object value)[] parameters)
+        {
+            var requestUri     = $"{BaseUri}{path}{string.Join("?", ConvertToParameter(parameters))}";
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+
+            var response = await _httpClient.SendAsync(requestMessage).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode) throw new NotImplementedException();
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            return content;
+        }
+
+        public async Task PostAsync(string path, params (string key, object value)[] parameters)
+        {
+            var requestUri = $"{BaseUri}{path}{string.Join("?", ConvertToParameter(parameters))}";
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri);
+
+            var response = await _httpClient.SendAsync(requestMessage);
+
+            if (!response.IsSuccessStatusCode) throw new NotImplementedException();
+        }
+
+        private IEnumerable<string> ConvertToParameter(IEnumerable<(string key, object value)> parameters)
+            => parameters != null && parameters.Any()
+                   ? parameters.Where(x => x.value != null).Select(x => $"{x.key}={x.value}")
+                   : Enumerable.Empty<string>();
+    }
+}
