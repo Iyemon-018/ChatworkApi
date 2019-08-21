2f1b8f0c91da42f89c0c32fa99f29ac69e564ff4
石井　健太 <kenta.ishii@motex.co.jp>
2019-08-21 17:20:58 +0900

    プロジェクトを新規追加

---
+++ ChatworkApi/Debugs/ChatworkApi.Debugs.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatworkApi.Debugs.Console
+{
+    using System.IO;
+    using Console = System.Console;
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var apiToken = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "api-token.txt"));
+            var apiClient = new ChatworkApi.ApiClient(apiToken);
+            var result = await apiClient.GetAsync($"/me");
+            
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ChatworkApi/sources/ChatworkApi/ApiClient.cs
@@ -0,0 +1,50 @@
+﻿namespace ChatworkApi
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public sealed class ApiClient
+    {
+        private static readonly string BaseUri = "https://api.chatwork.com/v2";
+
+        private static readonly string ApiTokenHeaderKey = "X-ChatWorkToken";
+
+        private readonly string _apiToken;
+
+        private readonly HttpClient _httpClient;
+
+        public ApiClient(string apiToken)
+        {
+            _apiToken   = apiToken;
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetAsync(string path, params (string key, string value)[] parameters)
+        {
+            var requestUri     = $"{BaseUri}{path}{string.Join("?", parameters.Select(x => $"{x.key}={x.value}"))}";
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            requestMessage.Headers.Add(ApiTokenHeaderKey, _apiToken);
+
+            var response = await _httpClient.SendAsync(requestMessage);
+
+            if (!response.IsSuccessStatusCode) throw new NotImplementedException();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return content;
+        }
+
+        public async Task PostAsync(string path, params (string key, string value)[] parameters)
+        {
+            var requestUri = $"{BaseUri}{path}{string.Join("?", parameters.Select(x => $"{x.key}={x.value}"))}";
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri);
+            requestMessage.Headers.Add(ApiTokenHeaderKey, _apiToken);
+
+            var response = await _httpClient.SendAsync(requestMessage);
+
+            if (!response.IsSuccessStatusCode) throw new NotImplementedException();
+        }
+    }
+}
