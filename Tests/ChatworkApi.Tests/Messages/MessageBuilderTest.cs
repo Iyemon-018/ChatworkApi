bd6f14e4a1218eae1c0e7cffab8f31fc047f459e
石井 健太 <i_m_a_walker@live.jp>
2019-08-28 12:42:08 +0000

    Merged PR 69: To メッセージを実装した。
    To メッセージを実装した。
    
    Related work items: #200

---
+++ Tests/ChatworkApi.Tests/Messages/MessageBuilderTest.cs
@@ -0,0 +1,94 @@
+﻿namespace ChatworkApi.Tests.Messages
+{
+    using System;
+    using ChatworkApi.Messages;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class MessageBuilderTest : TestBase
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="outputHelper">テスト結果を出力するためのプロバイダー</param>
+        public MessageBuilderTest(ITestOutputHelper outputHelper) : base(outputHelper)
+        {
+        }
+
+        #region To
+
+        [Fact]
+        public void Test_正常_To()
+        {
+            // arrange
+            var builder = new MessageBuilder();
+
+            // act
+            var actual = builder.To.Add(123456).Build();
+
+            // assert
+            Assert.Equal("[To:123456]", actual);
+            Output(actual);
+        }
+
+        [Fact]
+        public void Test_正常_To_Message付き()
+        {
+            // arrange
+            var builder = new MessageBuilder();
+
+            // act
+            var actual = builder.To.Add(9876543)
+                                   .AddMessage("講義のご連絡ありがとうございます。")
+                                   .Build();
+
+            // assert
+            Assert.Equal("[To:9876543]講義のご連絡ありがとうございます。", actual);
+            Output(actual);
+        }
+
+        [Fact]
+        public void Test_正常_To_複数回()
+        {
+            // arrange
+            var builder = new MessageBuilder();
+
+            // act
+            var actual = builder.To.Add(123)
+                                .To.Add(456)
+                                .AddMessage("test")
+                                .Build();
+
+            // assert
+            Assert.Equal("[To:123][To:456]test", actual);
+            Output(actual);
+        }
+
+        [Fact]
+        public void Test_正常_To_複数回_呼び出し順序()
+        {
+            // arrange
+            var builder = new MessageBuilder();
+
+            // act
+            var actual = builder.To.Add(123)
+                                .AddNewLine()
+                                .AddMessage($"こんにちは")
+                                .AddNewLine()
+                                .To.Add(1111)
+                                .AddNewLine()
+                                .To.Add(2222)
+                                .Build();
+
+            // assert
+            Assert.Equal($"[To:123]{Environment.NewLine}"
+                         + $"こんにちは{Environment.NewLine}"
+                         + $"[To:1111]{Environment.NewLine}"
+                         + $"[To:2222]"
+                       , actual);
+            Output(actual);
+        }
+
+        #endregion
+    }
+}
---
+++ sources/ChatworkApi/Messages/MessageBuilder.cs
@@ -0,0 +1,76 @@
+﻿namespace ChatworkApi.Messages
+{
+    using System.Text;
+
+    public sealed partial class MessageBuilder : IMessageBuilder, IToMessage, IReplyMessage
+    {
+        private readonly StringBuilder _message = new StringBuilder();
+
+        IMessageBuilder IMessageBuilder.AddMessage(string message)
+        {
+            _message.Append(message);
+
+            return this;
+        }
+
+        public IMessageBuilder AddNewLine()
+        {
+            _message.AppendLine();
+
+            return this;
+        }
+
+        public string Build() => _message.ToString();
+    }
+
+    public interface IToMessage
+    {
+        IMessageBuilder Add(int accountId);
+    }
+
+    public partial class MessageBuilder
+    {
+        public IToMessage To => this;
+
+        IMessageBuilder IToMessage.Add(int accountId)
+        {
+            _message.Append($"[To:{accountId}]");
+
+            return this;
+        }
+    }
+
+    public interface IReplyMessage
+    {
+        IMessageBuilder Add(int accountId
+                          , int roomId
+                          , int messageId);
+    }
+
+    public partial class MessageBuilder
+    {
+        public IReplyMessage Reply => this;
+
+        IMessageBuilder IReplyMessage.Add(int accountId
+                                        , int roomId
+                                        , int messageId)
+        {
+            _message.Append($"[rp aid={accountId} to={roomId}-{messageId}]");
+
+            return this;
+        }
+    }
+
+    public interface IMessageBuilder
+    {
+        IToMessage To { get; }
+
+        IReplyMessage Reply { get; }
+
+        string Build();
+
+        IMessageBuilder AddMessage(string message);
+
+        IMessageBuilder AddNewLine();
+    }
+}
