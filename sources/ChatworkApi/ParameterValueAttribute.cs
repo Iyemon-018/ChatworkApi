50cb5dd0c80a56c92d0673062e1c8d074866df39
石井 健太 <i_m_a_walker@live.jp>
2019-09-07 15:04:29 +0000

    Merged PR 87: Ver.0.9.0 コンフリクト解決結果をマージ
    Related work items: #187, #188, #190, #191, #196, #197, #198, #200, #201, #203, #204, #212, #213, #214, #215, #216, #217, #218, #219, #220

---
+++ sources/ChatworkApi/ParameterValueAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatworkApi
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class ParameterValueAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public ParameterValueAttribute(string value)
+        {
+            Value = value;
+        }
+    }
+}
